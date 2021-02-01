cc9145ac6e9f543e2b33ce66bacc5d010e7b8a18
DESKTOP-J67BSCC\DELL <kietnguyen1147@gmail.com>
2021-02-01 21:44:06 +0700

    src_code

---
+++ html/data/global.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'title': 'Frontend starter',
+  banner: require('./modules/banner1'),
+  imagesBase64: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC',
+  interJs: 'app.js',
+  rooms: require('./modules/rooms'),
+}
---
+++ html/data/modules/rooms.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+      name: 'GREEN',
+      url: '../assets/images/room_1.png',
+      id: '1'
+  },
+  {
+      name: 'BLUE',
+      url: '../assets/images/room_3.png',
+      id: '2'
+  },
+  {
+      name: 'VIOLET',
+      url: '../assets/images/room_2.png',
+      id: '3'
+  }]
+
+
